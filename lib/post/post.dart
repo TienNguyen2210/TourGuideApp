eed9a51d092aa99d39f8d95a715252c8407be442
eddie <eddiesunchao1999@gmail.com>
2024-04-29 21:57:26 -0700

    password recovery

--- lib/post/post.dart
+++ lib/post/post.dart
@@ -2,7 +2,7 @@ class Post {
   final String category;
   final double rating;
   final String description;
-  final String location; // Add location attribute
+  final String location;
 
   Post({required this.category, required this.rating, required this.description, required this.location});
 }
--- test/password_recovery_unit_test.dart
+++ test/password_recovery_unit_test.dart
@@ -1,41 +1,32 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mockito/mockito.dart';
-import 'package:firebase_auth/firebase_auth.dart';
-import 'package:tour_guide_app/pages/login_page.dart';
-import 'package:tour_guide_app/service/auth.dart';
+import 'package:tour_guide_app/pages/password_recovery.dart';
 
-class MockAuth extends Mock implements Auth {}
-class MockUserCredential extends Mock implements UserCredential {}
 class MockNavigatorObserver extends Mock implements NavigatorObserver {}
 
 void main() {
-  group('LoginScreen Tests', () {
-    late MockAuth mockAuth;
-    late MockNavigatorObserver mockNavigatorObserver;
-
-    setUp(() {
-      mockAuth = MockAuth();
-      mockNavigatorObserver = MockNavigatorObserver();
-    });
-
-    testWidgets('Sign in using email and password', (WidgetTester tester) async {
-      when(mockAuth.signInWithEmailAndPassword(
-          email: 'account@gmail.com', password: 'password123'))
-          .thenAnswer((_) async => MockUserCredential());
-
+  group('PasswordRecoveryScreen Widget Test', () {
+    testWidgets('Widget renders correctly', (WidgetTester tester) async {
       await tester.pumpWidget(MaterialApp(
-        home: LoginScreen(),
-        navigatorObservers: [mockNavigatorObserver],
+        home: PasswordRecoveryScreen(),
       ));
 
-      await tester.enterText(find.byType(TextField).at(0), 'account@gmail.com');
-      await tester.enterText(find.byType(TextField).at(1), 'password123');
+      expect(find.text('Password Recovery'), findsOneWidget);
+      expect(find.text('Enter your email'), findsOneWidget);
+      expect(find.text('Email'), findsOneWidget);
+      expect(find.text('Send Email'), findsOneWidget);
+    });
 
-      await tester.tap(find.widgetWithText(ElevatedButton, 'Sign In'));
-      await tester.pumpAndSettle();
+    testWidgets('Invalid email submission', (WidgetTester tester) async {
+      await tester.pumpWidget(MaterialApp(
+        home: PasswordRecoveryScreen(),
+      ));
 
-      verify(mockAuth.signInWithEmailAndPassword(email: 'account@gmail.com', password: 'password123')).called(1);
+      await tester.tap(find.text('Send Email'));
+      await tester.pump();
+
+      expect(find.text('             *Please enter Email'), findsOneWidget);
     });
   });
 }
