a01bc9a519a645e3b204099154f39809e0eb65cf
TienNguyen2210 <nguyentienmanh2003@gmail.com>
2024-04-29 21:17:35 -0700

    Integrate Google Maps and implement search function

--- lib/pages/signup_page.dart
+++ lib/pages/signup_page.dart
@@ -1,6 +1,5 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import 'package:tour_guide_app/pages/home_page.dart';
 import 'package:tour_guide_app/pages/user_profile_page.dart';
 import 'package:tour_guide_app/service/auth.dart';
 
