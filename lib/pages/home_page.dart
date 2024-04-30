203973dc9ae0d743a27bfe9ca21955114df6de8e
Kenneth Young <KenYoung727@gmail.com>
2024-04-29 19:52:23 -0700

    basic ui
    Need to change content and add spaces to make it look nicer

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatelessWidget {
@@ -5,8 +6,47 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text('Home Screen'),
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Current user location"),
+          backgroundColor: Colors.amber[800],
+          foregroundColor: Color.fromARGB(255, 83, 81, 81),
+        ),
+        body: Column(
+          children: [
+            Expanded(
+              child: ListView.builder(
+                scrollDirection: Axis.horizontal,
+                itemCount: 10,
+                itemBuilder: (context, index) {
+                  return Container(
+                    width: 160.0,
+                    child: Card(
+                      child: Text("Item no. $index"),
+                    ),
+                  );
+                },
+              ),
+            ),
+            Expanded(
+              child: ListView.builder(
+                scrollDirection: Axis.vertical,
+                itemCount: 10,
+                itemBuilder: (context, index) {
+                  return Container(
+                    height: 160.0,
+                    child: Card(
+                      child: Text("Item no. $index"),
+                    ),
+                  );
+                },
+              ),
+            )
+          ],
+        ),
+      ),
     );
   }
 }
