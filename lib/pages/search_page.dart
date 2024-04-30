ce7de03498047fdcf73d9c7c5e0b21a7fabb37b8
eddie <eddiesunchao1999@gmail.com>
2024-04-29 20:11:25 -0700

    tried

--- lib/pages/search_page.dart
+++ lib/pages/search_page.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
-class SearchSreen extends StatelessWidget {
-  const SearchSreen({super.key});
+class SearchScreen extends StatelessWidget {
+  const SearchScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
---
+++ lib/post/post.dart
@@ -0,0 +1,8 @@
+class Post {
+  final String category;
+  final double rating;
+  final String description;
+  final String location; // Add location attribute
+
+  Post({required this.category, required this.rating, required this.description, required this.location});
+}
