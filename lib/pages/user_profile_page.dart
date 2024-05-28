ecf5e92447da8e0cce62addbf15dfd4dbe8bfcd2
TienNguyen2210 <nguyentienmanh2003@gmail.com>
2024-05-28 01:16:13 -0700

    nothing

--- lib/pages/user_profile_page.dart
+++ lib/pages/user_profile_page.dart
@@ -7,6 +7,9 @@ class UserProfile extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        title: Text('User Profile'),
+      ),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
